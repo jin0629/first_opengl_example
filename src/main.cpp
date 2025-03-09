6ecfa4419f82cdf4a39d18cf207de8701f47a81f
jin0629git <jin0o0w@naver.com>
2025-03-09 14:56:30 +0900

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,53 @@
 #include <spdlog/spdlog.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv) {
     // 시작을 알리는 로그
     SPDLOG_INFO("Start program");
 
-       // glfw 라이브러리 초기화, 실패하면 에러 출력후 종료
-       SPDLOG_INFO("Initialize glfw");
-       if (!glfwInit()) {
-           const char* description = nullptr;
-           glfwGetError(&description);
-           SPDLOG_ERROR("failed to initialize glfw: {}", description);
-           return -1;
-       }
+    // glfw 라이브러리 초기화, 실패하면 에러 출력후 종료
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
 
-         // glfw 윈도우 생성, 실패하면 에러 출력후 종료
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
-    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    auto window = glfwCreateWindow( WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr); // 예시로 윈도우 크기와 이름을 설정
     if (!window) {
         SPDLOG_ERROR("failed to create glfw window");
         glfwTerminate();
         return -1;
     }
-  // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
-  SPDLOG_INFO("Start main loop");
-  while (!glfwWindowShouldClose(window)) {
-      glfwPollEvents();
-  }
 
-  glfwTerminate();
-  return 0;
+    glfwMakeContextCurrent(window);
+    
+    // glad를 활용한 OpenGL 함수 로딩
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+
+    auto glVersion = glGetString(GL_VERSION);
+    // GLubyte* 타입을 const char*로 변환하여 출력
+    SPDLOG_INFO("OpenGL context version: {}", reinterpret_cast<const char*>(glVersion));
+
+    // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
-
